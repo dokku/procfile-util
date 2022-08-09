11295f1476db6f09d7e3a2cee8c5d2738120b88a
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2022-08-08 21:25:05 -0400

    fix: move shorthand to second place

--- commands/flags.go
+++ commands/flags.go
@@ -13,10 +13,10 @@ type GlobalFlagCommand struct {
 }
 
 func (c *GlobalFlagCommand) GlobalFlags(f *flag.FlagSet) {
-	f.StringVarP(&c.procfile, "P", "procfile", "Procfile", "path to a procfile")
-	f.StringVarP(&c.delimiter, "D", "delimiter", ":", "delimiter in use within procfile")
-	f.IntVarP(&c.defaultPort, "d", "default-port", 5000, "default port to use")
-	f.BoolVarP(&c.strict, "S", "strict", false, "strictly parse the Procfile")
+	f.StringVarP(&c.procfile, "procfile", "P", "Procfile", "path to a procfile")
+	f.StringVarP(&c.delimiter, "delimiter", "D", ":", "delimiter in use within procfile")
+	f.IntVarP(&c.defaultPort, "default-port", "d", 5000, "default port to use")
+	f.BoolVarP(&c.strict, "strict", "S", false, "strictly parse the Procfile")
 }
 
 func (c *GlobalFlagCommand) AutocompleteGlobalFlags() complete.Flags {
